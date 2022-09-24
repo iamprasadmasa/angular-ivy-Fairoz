db14b14f5f85586a7103403a70c44018323296a7
iamprasadmasa <110100178+iamprasadmasa@users.noreply.github.com>
2022-09-24 15:19:26 +0530

    1 file added

---
+++ .vscode/hello.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appFzzz]',
+})
+export class helloDirective {
+  
+  fairoz: any={};
+  
+  
+  constructor(praivate el:ElementRef) {
+
+
+
+
+  }
+}
